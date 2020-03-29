e6f5a082404f0c6e6ea47c40a6caecdb94861e6c
Caleb335 <belac335@gmail.com>
2020-03-29 19:24:41 +0100

    codewars stask done

---
+++ app.js
@@ -0,0 +1,18 @@
+// Code goes here...
+const unoderedCapitalize = (word) => {
+    let result = '' //stores the string
+
+    for (let i = 0; i < word.length; i++) {
+        // condition for the string capitalization, when the indexing is either odd or even
+        if (i % 2 !== 0) {
+            result += word[i].toLowerCase()
+        } else {
+            result += word[i].toUpperCase()
+        }
+    }
+    return result
+}
+
+let yourName = 'ionwarez'
+console.log(unoderedCapitalize(yourName))
+
