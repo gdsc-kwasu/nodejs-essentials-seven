2afff31d17c51e2e8cea228d66db4734b5fe9918
Caleb335 <belac335@gmail.com>
2020-04-02 11:54:18 +0100

    prettier & eslint configs

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // Code goes here...
-const Capitalize = (word) => {
+const capitalize = (word) => {
     const split = word.split('') // separates elements (letters) in the word
-    
-    let firstWord = split.map((a, arrIndex) => 
-        arrIndex % 2 === 0 ?
-        a.toUpperCase() // capitalizes the "even" indexes of the array
-        : a
+
+    let firstWord = split.map(
+        (letter, arrIndex) =>
+            arrIndex % 2 === 0 ? letter.toUpperCase() : letter, // capitalizes the "even" indexes of the array
     )
 
-    let secondWord = split.map((a, arrIndex) =>  // creates a new array
-        arrIndex % 2 !== 0 ? 
-        a.toUpperCase() // capitalizes the "odd" indexes of the array
-        : a
+    let secondWord = split.map(
+        (
+            letter,
+            arrIndex, // creates a new array
+        ) => (arrIndex % 2 !== 0 ? letter.toUpperCase() : letter), // capitalizes the "odd" indexes of the array
     )
-    
+
     return [firstWord.join(''), secondWord.join('')]
 }
 
-console.log(Capitalize('ionwarez'))
+console.log(capitalize('ionwarez'))
