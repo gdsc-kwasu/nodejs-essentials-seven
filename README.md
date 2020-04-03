b7b77d6579afb328931f7b7d4456051dd13670ac
Caleb335 <belac335@gmail.com>
2020-04-03 11:25:07 +0100

    airbnb config, lint & format scripts

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nodejs-essentials-seven
 
+## Formatting & linting
+Run the script below, to format your code
+```npm 
+    npm run format
+```
+
+When you are running a script and do not want the extra noise, you can use the script below to test Eslint linting.
+```npm 
+    npm run lint --silent
+```
--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 // Code goes here...
 const capitalize = (word) => {
-    const split = word.split('') // separates elements (letters) in the word
+  const split = word.split(''); // separates elements (letters) in the word
 
-    let firstWord = split.map(
-        (letter, arrIndex) =>
-            arrIndex % 2 === 0 ? letter.toUpperCase() : letter, // capitalizes the "even" indexes of the array
-    )
+  const firstWord = split.map(
+    (letter, arrIndex) => (arrIndex % 2 === 0 ? letter.toUpperCase() : letter), // capitalizes the "even" indexes of the array
+  );
 
-    let secondWord = split.map(
-        (
-            letter,
-            arrIndex, // creates a new array
-        ) => (arrIndex % 2 !== 0 ? letter.toUpperCase() : letter), // capitalizes the "odd" indexes of the array
-    )
+  const secondWord = split.map(
+    (
+      letter,
+      arrIndex, // creates a new array
+    ) => (arrIndex % 2 !== 0 ? letter.toUpperCase() : letter), // capitalizes the "odd" indexes of the array
+  );
 
-    return [firstWord.join(''), secondWord.join('')]
-}
+  return [firstWord.join(''), secondWord.join('')];
+};
 
-console.log(capitalize('ionwarez'))
+console.log(capitalize('ionwarez'));
